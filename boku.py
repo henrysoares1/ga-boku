45c279f099cf3a8067aaf0eb7ae2a52fa053a8df
Henry <hspereira@inf.ufpel.edu.br>
2020-11-19 18:56:04 -0300

    Função que inicializa o tabuleiro feita

---
+++ boku.py
@@ -0,0 +1,35 @@
+
+
+def start_board(size):
+
+  board = []
+  new_row = []
+  row_elements =  5
+  flag = True         # flag pra saber se ele ta antes ou depois da metade do tabuleiro
+  row = size - 5
+  row = row*2+1      # a linha do meio do tabuleiro mais as linhas de cima e de baixo
+  
+  while row_elements >= 5:  
+    if row_elements < size and flag:
+      for i in range(0,row_elements):
+        new_row.append(0)
+      row_elements = row_elements +1
+    else:
+      for i in range(0,row_elements):
+        new_row.append(0)
+      row_elements = row_elements -1
+      flag = False
+    board.append(new_row)
+    new_row = []
+
+  return board
+
+def print_board(board):
+  for i in board:
+      print(i)
+
+  return 0
+
+
+tabuleiro = start_board(10)
+print_board(tabuleiro)
