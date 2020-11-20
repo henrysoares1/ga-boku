ebf0f9f59999d79ac7d804f41b04f947d98279d0
Gilberto Kreisler <gkreisler5@hotmail.com>
2020-11-20 09:40:34 -0300

    pequena enxugada no código

--- boku.py
+++ boku.py
@@ -4,19 +4,20 @@ def start_board(size):
 
   board = []
   new_row = []
-  row_elements =  5
+  row_elements = 5
   flag = True         # flag pra saber se ele ta antes ou depois da metade do tabuleiro
-  
-  while row_elements >= 5:  
+
+  while row_elements >= 5:
+    
+    for i in range(0, row_elements):
+        new_row.append(0)
+
     if row_elements < size and flag:
-      for i in range(0,row_elements):
-        new_row.append(0)
-      row_elements = row_elements +1
+        row_elements += 1
     else:
-      for i in range(0,row_elements):
-        new_row.append(0)
-      row_elements = row_elements -1
-      flag = False
+        row_elements -= 1
+        flag = False
+
     board.append(new_row)
     new_row = []
 
