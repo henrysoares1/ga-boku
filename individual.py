ff7a893d4808c7d2fd3b80af73743a3bb77e4825
Gilberto Kreisler <gkreisler5@hotmail.com>
2020-12-01 20:56:38 -0300

    metodo retornando em formato de par coordenada (x,y)

--- individual.py
+++ individual.py
@@ -10,6 +10,15 @@ class Individual:
 
         return chromo
 
+    def get_coordinates(self):
+        val = 15
+        x = self.chromosome & val
+        val <<= 4
+        y = self.chromosome & val
+        y >>= 4
+
+        return (x,y)
+
     def get_chromosome(self):
         return self.chromosome
 
@@ -21,6 +30,7 @@ class Individual:
 
 
 if __name__ == "__main__":
-    individuo = Individual(10,8)
+    individuo = Individual(15,1)
 
     print(individuo.get_chromosome())
+    print(individuo.get_coordinates())
