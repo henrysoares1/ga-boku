f52b20061ff612db9c38a8738af3eed71f4bec80
Gilberto Kreisler <gkreisler5@hotmail.com>
2020-12-13 19:03:49 -0300

    adicionado suporte para o sanduiche no indivíduo

--- individual.py
+++ individual.py
@@ -1,8 +1,22 @@
 
 class Individual:
     def __init__(self, x: int, y: int):
-        self.chromosome = self.__compose_chromosome(x,y)
-        self.fitness = 0
+        self.__chromosome = self.__compose_chromosome(x,y)
+        self.__fitness = 0
+        self.__is_sandwich = False
+        self.__sandwich_coords = []
+
+    def set_is_sandwich(self):
+        self.__is_sandwich = True
+
+    def get_is_sandwich(self):
+        return self.__is_sandwich
+
+    def add_sandwiched_enemies(self, x: int, y: int):
+        self.__sandwich_coords.append((x,y))
+
+    def get_sandwiched_enemies(self):
+        return self.__sandwich_coords
 
     def __compose_chromosome(self, x: int, y: int):
         chromo = y
@@ -13,28 +27,21 @@ class Individual:
 
     def get_coordinates(self):
         val = 31
-        x = self.chromosome & val
+        x = self.__chromosome & val
         val <<= 5
-        y = self.chromosome & val
+        y = self.__chromosome & val
         y >>= 5
 
         return (x, y)
 
     def get_chromosome(self):
-        return self.chromosome
+        return self.__chromosome
 
     def set_chromosome(self, chromo):
-        self.chromosome = chromo
+        self.__chromosome = chromo
 
     def get_fitness(self):
-        return self.fitness
+        return self.__fitness
 
     def set_fitness(self, fitness):
-        self.fitness = fitness
-
-
-if __name__ == "__main__":
-    individuo = Individual(15,1)
-
-    print(individuo.get_chromosome())
-    print(individuo.get_coordinates())
+        self.__fitness = fitness
